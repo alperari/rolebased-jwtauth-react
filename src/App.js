131c3c34738def33984c8431bad621e81195f465
ekinbeyazit <ekinbeyazit@sabanciuniv.edu>
2023-03-30 23:13:22 +0300

    animations

---
+++ src/App.js
@@ -0,0 +1,63 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+
+  )
+
+
+
+}
+
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout></Layout>,
+    children:[
+      {
+        path:"/",
+        element:<Home></Home>
+      },
+      {
+        path:"/products/:id",
+        element:<Products></Products>
+      },
+      {
+        path:"/product/:id",
+        element:<Product></Product>
+      }
+
+    ]
+  }
+
+
+])
+
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <a>Footer</a>
+    )
+}
+
+export default Footer
