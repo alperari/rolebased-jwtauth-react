2e2a188b83ca3b410d0d2ab58014b479f01df07c
Alper ARI <alper.hamza.ari@gmail.com>
2023-04-05 18:00:47 +0300

    Implement comment display on VerticalProductCard

--- src/components/Product/VerticalProductCard.js
+++ src/components/Product/VerticalProductCard.js
@@ -13,10 +13,95 @@ import {
   ToggleSwitch,
   Select,
   Rating,
+  Spinner,
 } from 'flowbite-react';
+
+import { RatingService } from '../../services/RatingService';
 
 const VerticalProductCard = ({ product }) => {
   const [ratings, setRatings] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchRatings = async () => {
+    setLoading(true);
+    const fetchedRatings = await RatingService.getRatingsByProductId({
+      productId: product._id,
+    });
+
+    setRatings(fetchedRatings);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchRatings();
+  }, []);
+
+  const FilledStars = (number) => {
+    const stars = [];
+    for (let i = 0; i < number; i++) {
+      stars.push(<Rating.Star />);
+    }
+    return stars;
+  };
+
+  const EmptyStars = (number) => {
+    const stars = [];
+    for (let i = 0; i < number; i++) {
+      stars.push(<Rating.Star filled={false} />);
+    }
+    return stars;
+  };
+
+  const Ratings = () => {
+    if (loading)
+      return (
+        <div class="flex flex-row gap-2">
+          <Spinner color="failure" aria-label="Failure spinner example" />
+          Loading Comments...
+        </div>
+      );
+
+    if (ratings.length === 0) {
+      return (
+        <p className="text-gray-500 dark:text-gray-400">
+          <Rating>
+            <Rating.Star filled={false} />
+            <Rating.Star filled={false} />
+            <Rating.Star filled={false} />
+            <Rating.Star filled={false} />
+            <Rating.Star filled={false} />
+            <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
+              No Ratings
+            </p>
+          </Rating>
+        </p>
+      );
+    } else {
+      // Calculate average rating
+      const total = ratings.reduce((acc, rating) => acc + rating.stars, 0);
+      const average = total / ratings.length;
+
+      const roundedAverage = Math.round(average * 10) / 10;
+
+      const filledNo = roundedAverage;
+      const emptyNo = 5 - filledNo;
+
+      const filledStars = FilledStars(filledNo);
+      const emptyStars = EmptyStars(emptyNo);
+
+      return (
+        <div>
+          <Rating>
+            {filledStars.map((star) => star)}
+            {emptyStars.map((star) => star)}
+            <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
+              {average} / 5
+            </p>
+          </Rating>
+        </div>
+      );
+    }
+  };
 
   return (
     <div className="max-w-sm">
@@ -30,16 +115,7 @@ const VerticalProductCard = ({ product }) => {
           </h5>
         </a>
 
-        <Rating>
-          <Rating.Star />
-          <Rating.Star />
-          <Rating.Star />
-          <Rating.Star />
-          <Rating.Star filled={false} />
-          <p className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-400">
-            4.95 out of 5
-          </p>
-        </Rating>
+        {Ratings()}
 
         <div className="flex items-center justify-between">
           <span className="text-3xl font-bold text-gray-900 dark:text-white">
