42a836463d2225bd39fda01b31664a3a81b3de84
ekinbeyazit <ekinbeyazit@sabanciuniv.edu>
2023-04-14 15:35:13 +0300

    cart page using homepage products for now (no button functionality)

--- src/pages/Cart/CartPage.jsx
+++ src/pages/Cart/CartPage.jsx
@@ -1,8 +1,70 @@
+import React, { useEffect, useState } from 'react';
+import { Card, Button, Label, TextInput, Checkbox } from 'flowbite-react';
+import { Link, useNavigate } from 'react-router-dom';
+import { ProductService } from '../../services/ProductService';
+import { RatingService } from '../../services/RatingService';
+import HorizontalProductCard from '../../components/Product/HorizontalProductCard';
 const CartPage = () => {
-    return(
-    <div className="text-red-400">
-        Hello
-    </div>)
+    
+    
+    const [loading, setLoading] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [productsCopy, setProductsCopy] = useState([]);
+    const [query, setQuery] = useState('');
+    const [onlyStocksCheck, setOnlyStocksCheck] = useState(false);
+    
+    const navigate = useNavigate();
+
+    const fetchProducts = async () => {
+        setLoading(true);
+
+        // Fetch products
+        const fetchedProducts = await ProductService.getProductsWithRatings();
+
+        setProducts(fetchedProducts);
+        setProductsCopy(fetchedProducts);
+        setLoading(false);
+    };
+    
+    
+    const ProductsGridView = () => {
+        if (loading) {
+        return <div class="mt-12">Loading...</div>;
+        } else {
+        if (products.length === 0) {
+            return <div class="mt-12">No products found.</div>;
+        } else {
+            return (
+            <div class="mt-5 grid grid-rows gap-3 ">
+                {products.map((product, index) => {
+                return (
+                    <HorizontalProductCard
+                    product={product}
+                    setProducts={setProducts}
+                    key={index}
+                    />
+                );
+                })}
+            </div>
+            );
+        }
+        }
+    };
+
+
+    
+
+    useEffect(() => {
+        fetchProducts();
+      }, []);
+
+    return (
+    <div class="flex flex-col items-center mx-64 my-8">
+        
+        
+        <ProductsGridView />
+    </div>
+    );
     
 }
 export default CartPage;
