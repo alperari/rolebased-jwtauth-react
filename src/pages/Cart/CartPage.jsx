9d570eb78eb3ef840973d43fb1dad66bd1e94d52
Alper ARI <alper.hamza.ari@gmail.com>
2023-04-16 23:45:22 +0300

    Improve CartPage: SubTotal & Discount & Total

--- src/pages/Cart/CartPage.jsx
+++ src/pages/Cart/CartPage.jsx
@@ -108,18 +108,44 @@ const CartPage = () => {
         <div class="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3">
           <div class="mb-2 flex justify-between">
             <p class="text-gray-700">Subtotal</p>
-            <p class="text-gray-700">$129.99</p>
+            <p class="text-gray-700 font-semibold">
+              $
+              {cartState.products.reduce(
+                (acc, item) => acc + item.price * item.cartQuantity,
+                0
+              )}
+            </p>
+          </div>
+          <div class="mb-2 flex justify-between">
+            <p class="text-gray-700">Discount</p>
+            <p class="text-green-500 font-semibold">
+              - $
+              {cartState.products.reduce((acc, item) => {
+                const discount =
+                  acc + (item.price * item.cartQuantity * item.discount) / 100;
+                return discount;
+              }, 0)}
+            </p>
           </div>
           <div class="flex justify-between">
             <p class="text-gray-700">Shipping</p>
-            <p class="text-gray-700">$4.99</p>
+            <p class="text-gray-700 font-semibold">$4.99</p>
           </div>
           <hr class="my-4" />
           <div class="flex justify-between">
             <p class="text-lg font-bold">Total</p>
             <div class="">
-              <p class="mb-1 text-lg font-bold">$134.98 USD</p>
-              <p class="text-sm text-gray-700">including VAT</p>
+              <p class="mb-1 text-lg font-bold">
+                $
+                {cartState.products.reduce((acc, item) => {
+                  const discount =
+                    acc +
+                    (item.price * item.cartQuantity * item.discount) / 100;
+
+                  const newPrice = item.price * item.cartQuantity - discount;
+                  return newPrice;
+                }, 0) + 4.99}
+              </p>
             </div>
           </div>
           <button class="mt-6 w-full rounded-md bg-blue-500 py-1.5 font-medium text-blue-50 hover:bg-blue-600">
