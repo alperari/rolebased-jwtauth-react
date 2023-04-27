08136b3f2a65e94489cc3ab554a537cd69baac79
Alper ARI <alper.hamza.ari@gmail.com>
2023-04-27 17:07:57 +0300

    Improve RefundPage (salesManager)

--- src/pages/Refunds/RefundsPage.jsx
+++ src/pages/Refunds/RefundsPage.jsx
@@ -21,6 +21,7 @@ import { parseDateTime, groupBy } from '../../helpers/helperFunctions';
 const user = JSON.parse(localStorage.getItem('user'));
 
 const RefundsPageAsSalesManager = () => {
+  const [loading, setLoading] = useState(false);
   const [refunds, setRefunds] = useState([]);
   const [groupedRefunds, setGroudpedRefunds] = useState({
     pending: [],
@@ -30,7 +31,159 @@ const RefundsPageAsSalesManager = () => {
 
   const navigate = useNavigate();
 
-  return <div>sales manager</div>;
+  const fetchMyRefunds = async () => {
+    setLoading(true);
+
+    const fetchedRefunds = await RefundService.getMyRefunds();
+
+    if (fetchedRefunds.error) {
+      alert(fetchedRefunds.error);
+      return;
+    }
+
+    // Group refunds by status
+    const groupedRefunds = groupBy(fetchedRefunds, 'status');
+
+    // Set grouped refunds
+    setGroudpedRefunds(groupedRefunds);
+
+    setRefunds(fetchedRefunds);
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMyRefunds();
+  }, []);
+
+  const CustomTable = ({ refunds }) => {
+    if (!refunds || refunds.length === 0) {
+      return (
+        <div>
+          <h1>No refunds found</h1>
+        </div>
+      );
+    } else {
+      return (
+        <div class="w-screen px-12">
+          <Table hoverable={true}>
+            <Table.Head>
+              <Table.HeadCell></Table.HeadCell>
+              <Table.HeadCell>Product</Table.HeadCell>
+              <Table.HeadCell>Order</Table.HeadCell>
+
+              <Table.HeadCell>Request Date</Table.HeadCell>
+
+              <Table.HeadCell>Total</Table.HeadCell>
+
+              <Table.HeadCell>Status</Table.HeadCell>
+            </Table.Head>
+
+            <Table.Body className="divide-y">
+              {refunds.map((refund) => {
+                const [date, time] = parseDateTime(refund.date);
+
+                return (
+                  <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                    <Table.Cell width="100px">
+                      <img
+                        src={refund.productDetails.imageURL}
+                        alt={refund.productDetails.name}
+                      />
+                    </Table.Cell>
+                    <Table.Cell>
+                      <Link
+                        to="/product"
+                        state={{ product: refund.productID }}
+                        className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                      >
+                        {refund.productID}
+                      </Link>
+                    </Table.Cell>
+
+                    <Table.Cell>
+                      <Link
+                        to={'/order/' + refund.orderID}
+                        className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                      >
+                        {refund.orderID}
+                      </Link>
+                    </Table.Cell>
+
+                    <Table.Cell>
+                      {date} {time}
+                    </Table.Cell>
+
+                    <Table.Cell>
+                      <span class="font-semibold">
+                        ${refund.quantity * refund.price}
+                      </span>
+                    </Table.Cell>
+
+                    <Table.Cell>
+                      {refund.status === 'pending' ? (
+                        <div class="flex flex-row gap-3">
+                          <Button onClick={() => {}} color="green" size="sm">
+                            Approve
+                          </Button>
+
+                          <Button onClick={() => {}} color="red" size="sm">
+                            Reject
+                          </Button>
+                        </div>
+                      ) : refund.status == 'approved' ? (
+                        <div class="text-green-400 tracking-tight font-bold">
+                          Approved
+                        </div>
+                      ) : (
+                        <div class="text-red-400 tracking-tight font-bold">
+                          Rejected
+                        </div>
+                      )}
+                    </Table.Cell>
+                  </Table.Row>
+                );
+              })}
+            </Table.Body>
+          </Table>
+        </div>
+      );
+    }
+  };
+
+  const CustomTabs = () => {
+    return (
+      <div class="flex flex-row items-center justify-center">
+        <Tabs.Group
+          aria-label="Tabs with icons"
+          style="underline"
+          class="flex flex-row items-center gap-3 justify-center"
+        >
+          <Tabs.Item active={true} title="Pending" icon={TbClockHour8}>
+            <CustomTable refunds={groupedRefunds.pending} />
+          </Tabs.Item>
+          <Tabs.Item title="Approved" icon={TbThumbUp}>
+            <CustomTable refunds={groupedRefunds.approved} />
+          </Tabs.Item>
+          <Tabs.Item title="Rejected" icon={TbThumbDown}>
+            <CustomTable refunds={groupedRefunds.rejected} />
+          </Tabs.Item>
+        </Tabs.Group>
+      </div>
+    );
+  };
+
+  return (
+    <div class="flex flex-col mx-32 my-24">
+      <span class="font-bold text-center mb-8">My Refunds</span>
+
+      {loading ? (
+        <div class="flex flex-row justify-center items-center">Loading...</div>
+      ) : (
+        <CustomTabs />
+      )}
+    </div>
+  );
 };
 
 const RefundsPageAsCustomer = () => {
